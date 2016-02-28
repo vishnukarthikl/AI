82ed91159a26172a0a9a246a50216ea0a0f865a0
Vishnu karthik <vishnukl@usc.edu>
2016-02-27 20:25:36 -0800

    extracting constants

--- hw2/hw2cs561s16.py
+++ hw2/hw2cs561s16.py
@@ -1,5 +1,11 @@
 import re
 import sys
+from itertools import chain, imap
+
+
+def flatmap(f, items):
+    return list(chain.from_iterable(imap(f, items)))
+
 
 IMPLICATION = '=>'
 
@@ -22,9 +28,23 @@ class Sentence:
     def __str__(self):
         return self.s
 
+    def constants(self):
+        return filter(lambda x: x.istitle(), self.variables)
+
 
 def to_sentences(s):
     return map(lambda x: Sentence(x), strip(s.split('&&')))
+
+
+class KnowledgeBase:
+    def __init__(self):
+        self.knowledges = []
+        self.constants = []
+
+    def add_knowledge(self, knowledge):
+        self.knowledges.append(knowledge)
+        new_constants = knowledge.constants()
+        self.constants += filter(lambda x: x not in self.constants, new_constants)
 
 
 class Knowledge:
@@ -36,19 +56,28 @@ class Knowledge:
             self.premise = to_sentences(parts[0])
             self.conclusion = to_sentences(parts[1])
         else:
+            self.premise = []
             self.conclusion = to_sentences(parts[0])
 
     def __str__(self):
         return self.knowledge
 
+    def all_sentences(self):
+        sentences = []
+        if len(self.premise) > 0:
+            sentences += self.premise
+        sentences += self.conclusion
+        return sentences
+
+    def constants(self):
+        return flatmap(lambda x: x.constants(), self.all_sentences())
+
 
 input_file = sys.argv[2]
-sentences = []
+kb = KnowledgeBase()
 with open(input_file, 'r') as fin:
     query = fin.readline()
     for _ in range(int(fin.readline().strip())):
-        sentence = Knowledge(fin.readline())
-        sentences.append(sentence)
+        kb.add_knowledge(Knowledge(fin.readline()))
 
-for sentence in sentences:
-    print sentence.conclusion[0]
+print kb.constants
