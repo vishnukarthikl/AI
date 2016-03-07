8904e8b134f3900d6436be8c068b4ad384c17dac
Vishnu karthik <vishnukl@usc.edu>
2016-03-06 17:05:48 -0800

    standardizing variables (test case 1,2,3) works

--- hw2/hw2cs561s16.py
+++ hw2/hw2cs561s16.py
@@ -61,7 +61,7 @@ class Sentence:
         self.variables = strip(search.group(3).split(','))
 
     def __str__(self):
-        return self.s
+        return self.predicate + "(" + ','.join(self.variables) + ")"
 
     def has_variables(self):
         return len(self.variables) - len(self.constants()) > 0
@@ -87,7 +87,7 @@ class KnowledgeBase:
         new_constants = knowledge.constants()
         self.constants += filter(lambda x: x not in self.constants, new_constants)
 
-    def fetch_goals(self, query):
+    def fetch_rules(self, query):
         goals = []
         for knowledge in self.knowledges:
             if self.can_unify(knowledge, query):
@@ -119,7 +119,11 @@ class Knowledge:
             self.conclusion = Sentence(parts[0])
 
     def __str__(self):
-        return self.knowledge
+        if len(self.premise) == 0:
+            return self.conclusion.__str__()
+        else:
+            return " && ".join(
+                    map(lambda x: x.__str__(), self.premise)) + " " + IMPLICATION + " " + self.conclusion.__str__()
 
     def all_sentences(self):
         sentences = []
@@ -136,25 +140,55 @@ class InferenceResolver:
     def __init__(self, knowledge_base):
         self.kb = knowledge_base
         self.logger = Logger()
+        self.random_variables = 0
+
+    def generate_variable(self):
+        self.random_variables += 1
+        return "z" + str(self.random_variables)
 
     def resolve(self, queries):
         if len(queries) == 1:
-            query = queries[0]
-            goals = self.kb.fetch_goals(query)
-            goals = map(lambda x: self.replace_query(x, query), goals)
-            for _ in self.fol_or(goals):
-                return True
+            for theta in self.fol_or(queries[0], {}):
+                if theta:
+                    return True
         else:
             return False
 
-    def fol_or(self, goals):
-        for goal in goals:
-            success, theta = self.validate(goal)
-            if success:
+    def fol_or(self, goal, theta):
+        rules = self.kb.fetch_rules(goal)
+        rules = self.standardize(rules, goal, theta)
+        self.logger.log(stringify(self.substitute(theta, goal), "Ask"))
+        for rule in rules:
+            # self.logger.log(stringify(self.substitute(theta, goal), "Ask"))
+            for theta in self.fol_and(rule.premise, self.unify(rule.conclusion, goal, deepcopy(theta)),
+                                      rule.conclusion):
+                self.logger.log(stringify(self.substitute(theta, rule.conclusion), "True"))
                 yield theta
+        self.logger.log(stringify(self.substitute(theta, goal), "False"))
 
-    def validate(self, goal):
-        pass
+    def fol_and(self, goals, theta, parent):
+        if len(goals) == 0:
+            yield theta
+        else:
+            first, rest = goals[0], goals[1:]
+            for theta1 in self.fol_or(self.substitute(theta, first), deepcopy(theta)):
+                for theta2 in self.fol_and(rest, deepcopy(theta1), parent):
+                    yield theta2
+            self.logger.log(stringify(self.substitute(theta, parent), "Ask"))
+
+    def unify(self, x, y, theta):
+        theta = deepcopy(theta)
+        if x.predicate != y.predicate:
+            return None
+        for i, x_variable in enumerate(x.variables):
+            y_variable = y.variables[i]
+            if is_constant(x_variable) and is_constant(y_variable) and x_variable != y_variable:
+                return None
+            elif is_variable(x_variable) and is_constant(y_variable) and x_variable not in theta:
+                theta[x_variable] = y_variable
+            elif is_variable(y_variable) and is_constant(x_variable) and y_variable not in theta:
+                theta[y_variable] = x_variable
+        return theta
 
     def replace_query(self, goal, query_sentence):
         replace_map = {}
@@ -170,6 +204,39 @@ class InferenceResolver:
                     predicate.variables[i] = replace_map[variable]
 
         return new_goal
+
+    def substitute(self, theta, sentence):
+        substituted_sentence = deepcopy(sentence)
+        for i, variable in enumerate(sentence.variables):
+            if is_variable(variable) and variable in theta:
+                substituted_sentence.variables[i] = theta[variable]
+        return substituted_sentence
+
+    def standardize(self, rules, goal, theta):
+        standardized_rules = []
+        for rule in rules:
+            variable_changes = {}
+            rule = deepcopy(rule)
+            for i, variable in enumerate(goal.variables):
+                conclusion_variable = rule.conclusion.variables[i]
+                if is_variable(variable) and is_variable(conclusion_variable) and conclusion_variable != variable:
+                    variable_changes[conclusion_variable] = variable
+                    rule.conclusion.variables[i] = variable
+                elif (is_variable(conclusion_variable) and conclusion_variable in theta) or (
+                    is_variable(conclusion_variable) and conclusion_variable in goal.variables):
+                    rule.conclusion.variables[i] = self.generate_variable()
+                    variable_changes[conclusion_variable] = rule.conclusion.variables[i]
+
+            for premise in rule.premise:
+                for i, premise_variable in enumerate(premise.variables):
+                    if premise_variable in goal.variables and premise_variable not in variable_changes:
+                        variable_changes[premise_variable] = self.generate_variable()
+                    if premise_variable in variable_changes:
+                        premise.variables[i] = variable_changes[premise_variable]
+
+            standardized_rules.append(rule)
+
+        return standardized_rules
 
 
 input_file = sys.argv[2]
