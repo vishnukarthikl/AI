ec694fbbcd8ab4edd3162c4e5e38d6e63883d964
Vishnu karthik <vishnukl@usc.edu>
2016-04-11 11:24:12 -0700

    recursively computing parent probability

--- hw3/hw3cs561s16.py
+++ hw3/hw3cs561s16.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 import sys
 
@@ -45,27 +46,32 @@ class ProbabiltyQuery():
 class Node():
     def __init__(self, lines):
         self.given = []
-        self.probability = {}
+        self.probability = {True: {}, False: {}}
         self.set_names(lines)
         self.populate_probability(lines[1:])
 
     def populate_probability(self, lines):
-        if self.standalone():
-            self.probability["*"] = float(lines[0])
+        if self.is_standalone():
+            self.probability[True] = float(lines[0])
+            self.probability[False] = 1 - float(lines[0])
         else:
             for line in lines:
                 split = line.split(" ")
                 p = float(split[0])
                 occurence = "".join(split[1:])
-                self.probability[occurence] = p
+                self.probability[True][occurence] = p
+                self.probability[False][occurence] = 1 - p
 
-    def standalone(self):
+    def get_probability(self, occurence):
+        return self.probability[occurence]
+
+    def is_standalone(self):
         return len(self.given) == 0
 
     def set_names(self, lines):
         event = lines[0]
         split = event.split("|")
-        self.name = split[0]
+        self.name = split[0].strip()
         if len(split) > 1:
             self.given = split[1].strip().split(" ")
 
@@ -84,7 +90,32 @@ class BayesNet:
         self.nodes[node.name] = node
 
     def process(self, q):
-        pass
+        if not q.given:
+            return reduce(lambda acc, t: acc * self.calculate(t.name, t.occurence), q.to_calculate, 1)
+
+    def generate_bool_table(self, size):
+        permutation = list(itertools.product(*["-+"] * size))
+        return map(lambda x: ''.join(map(str, x)), permutation)
+
+    def calculate(self, event, occurence):
+        node = self.nodes[event]
+        if node.is_standalone():
+            return node.get_probability(occurence)
+        probability_table = node.get_probability(occurence)
+        result = 0
+        for parent_occurence, parent_probability in probability_table.iteritems():
+            product = parent_probability
+            combinations = zip(list(parent_occurence), node.given)
+            for combination in combinations:
+                product *= self.calculate(combination[1], self.convert(combination[0]))
+            result += product
+        return result
+
+    def convert(self, str):
+        if str == "+":
+            return True
+        else:
+            return False
 
 
 class Event:
@@ -116,10 +147,9 @@ def get_nodes(lines):
     nodes = []
     while True:
         node, lines = get_till(lines, "***")
+        nodes.append(node)
         if len(lines) == 0:
             return nodes
-        else:
-            nodes.append(node)
 
 
 input_file = sys.argv[2]
@@ -137,3 +167,4 @@ for node in nodes:
     bayesNet.add(node)
 
 result = map(lambda q: bayesNet.process(q), queries)
+print result
